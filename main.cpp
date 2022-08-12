89f50248abee4b3c756f4add5fe79acd2ef32af7
trondarild <trondarild@gmail.com>
2022-08-12 20:32:22 +0200

    minimal filled circle

--- main.cpp
+++ main.cpp
@@ -21,22 +21,24 @@
 #include <OpenGL/glu.h>
 #include <GLUT/glut.h>
 
+
 //#endif
 #include "tat_graphics.h"
 
 
-float i = 0;
+float t = 0;
 void setup()
 {
     glutInitDisplayMode(GLUT_DOUBLE);
     glutInitWindowSize(300, 300);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Test");
+    
 }
 
 void update()
 {
-    i -= 1;
+    t += 1;
     glutPostRedisplay();
 }
 
@@ -46,35 +48,14 @@ void draw()
     glClear(GL_COLOR_BUFFER_BIT);
     glEnable(GL_LINE_SMOOTH);
 
-    glPushMatrix();
-    //glScalef(0.5, 0.5, 0.5);
-    //glTranslatef(0.1f, 0.1f, 0);
     glLineWidth(4.f);
     glPushMatrix();
-        glTranslatef(cos(0.1*i), -0.25f, 0);
-        //glRotatef(i, 0, 0, 1);
-        glColor3f(0.90f, 0.350f, 0.90f);
-        //glRectf(0, 0, 0.5, 0.5);
-        circle(1, -1, 0.5, 100);
+        glColor3f(0.80f, 0.80f, 0.80f);
+        circle(0, 0, 0.25);
     glPopMatrix();
-
-    glPushMatrix();
-        glTranslatef(-0.25, -0.25, 0);
-        glColor3f(0.9, 0.9, 0.2);
-        rect(0, 0, 0.5, 0.5);
-    glPopMatrix();
-
-    glPushMatrix();
-        //glTranslatef(-0.25, -0.25, 0);
-        glColor3f(0.9, 0.5, 0.2);
-        line(-0.5,-0.50, 0.5, 0.5);
-    glPopMatrix();
-    
-    glPopMatrix(); // scale
 
     glFlush();
     glutSwapBuffers();
-    //i+=1;
 }
 
 int main(int argc, char** argv)
